dec7887d55fcd6c2b8dcc94631ba139397393496
haliwt <jonewang@126.com>
2022-08-19 09:20:09 +0800

    Low Power is less 30uA is success .DATA.2022.08.19

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -121,7 +121,7 @@ int main(void)
                run_t.passwordsMatch =0;
                run_t.password_unlock =2;
                run_t.unLock_times=1;
-               run_t.gTimer_2s=2;
+               run_t.gTimer_2s=1;
 			   run_t.lowPower_flag=0; //low power flag
   			  POWER_ON();
            
@@ -152,11 +152,7 @@ int main(void)
 
       }
 
-	 
-     
-     
-     
-        if(run_t.passwordsMatch==0 && run_t.panel_lock==0){
+	 if(run_t.passwordsMatch==0 && run_t.panel_lock==0){
 		if(I2C_Read_From_Device(SC12B_ADDR,0x08,SC_Data,2)==DONE){
         // if(I2C_Simple_Read_From_Device(SC12B_ADDR,SC_Data,2) ==DONE){
 			KeyValue =(uint16_t)(SC_Data[0]<<8) + SC_Data[1];
@@ -176,15 +172,17 @@ int main(void)
 			}
 		}
 		 
-	  if(run_t.passwordsMatch ==1 && run_t.adminiId !=1){
+	    if(run_t.passwordsMatch ==1 && run_t.adminiId !=1){
 		  
-			RunCommand_Unlock();
+		  run_t.passwordsMatch=0;
+          run_t.gTimer_8s=0;
+          RunCommand_Unlock();
             
 	  }
 	  if(run_t.password_unlock==2){ //lock turn on Open 
-	  
+	       run_t.gTimer_8s =0;
 		  //set up flag permit to save data to EEPROM
-		  if(run_t.getKey == 0x01){
+		  if(run_t.getKey == 0x01 ){
 			   run_t.getKey = 0;
 			  run_t.Confirm_newPassword = 1;
 			  run_t.Numbers_counter=0;
@@ -201,30 +199,36 @@ int main(void)
 		  }
 		  //To save data to EEPROM
 		  if(run_t.Confirm_newPassword ==1 && run_t.adminiId==1){
-			 
+			 run_t.gTimer_8s=0;
 			 SavePassword_To_EEPROM();
 			  
 			  
 		  }
 		  //return to home position
-		  if(run_t.unLock_times==1 && run_t.adminiId==0){ //if(run_t.gTimer_2s ==2 && run_t.unLock_times==1 && run_t.Confirm_newPassword == 0){
-  
-				   if(run_t.gTimer_2s > 1){
+		  if(run_t.unLock_times==1 && run_t.adminiId==0){ //if(run_t.gTimer_2s ==2 && run_t.unLock_times==1 && run_t.Confirm == 0){
+                   run_t.gTimer_8s =0;
+				   POWER_ON();
+				   if(run_t.gTimer_2s > 3){
+				   	   run_t.powerOn=2;
 					   Motor_CW_Run();// Close 
-					   HAL_Delay(2115);//__delay_ms(2115);//(815);
+					   HAL_Delay(2120);//2010//(815);
 					   Motor_Stop();
-					   HAL_Delay(1000);// __delay_ms(1000);
 					   run_t.unLock_times =0;
-					    for(i=0;i<6;i++){ //WT.EDIT .2022.08.13
+					   for(i=0;i<6;i++){ //WT.EDIT .2022.08.13
 						  
 					       pwd1[i]=0;
 						   Readpwd[i]=0;
 					   }
+
+					  run_t.unLock_times=0;//WT.EDIT 2022.08.18
+					  run_t.password_unlock=0;
                    }
+
+			
+				  
 			  }
   
 		  }
-	  
 	  
 		 
 		  DispLed_Fun();
